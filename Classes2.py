baa0c9f7ebd0f4ccbecc752abca29ef072ca559b
shashank-ray <shashank.ray@zemosolabs.com>
2023-11-08 17:48:27 +0530

    added classes 2 exercise

---
+++ Classes2.py
@@ -0,0 +1,20 @@
+class Calculator:
+    def __init__(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+        
+    def add(self):
+        return self.num1 + self.num2
+
+    def subtract(self):
+        return self.num1 - self.num2
+        
+    def multiply(self):
+        return self.num1 * self.num2
+        
+    def divide(self):
+        self.num1 / self.num2
+
+calculate = Calculator(15,25)
+
+print(calculate.add())
